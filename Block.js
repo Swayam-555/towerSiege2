022d393d63d7ec54ba43b7627134970d68d5364f
Swayam-555 <radheshyam.panigrahi@gmail.com>
2021-05-08 13:25:02 +0530

    tower#2

--- Block.js
+++ Block.js
@@ -16,20 +16,7 @@ class Block{
       var pos= this.body.position;
       var angle = this.body.angle;
      
-      if(this.body.speed < 3){
-        rectMode(CENTER);
-        rect(pos.x,pos.y,this.width, this.height); 
-        
-        
-      }
-      else{
-        World.remove(world,this.body);
-       this.visibility = this.visibility-5;
-       push()
-      
-         tint(255,this.visibility);
-         image(this.image,this.body.position.x,this.body.position.y,50,50);
-         pop()
-      }
+      if(this.body.speed < 3){ push(); angleMode(RADIANS); translate(pos.x,pos.y); rotate(angle); rectMode(CENTER); rect(0,0,this.width, this.height); pop(); } else{ World.remove(world,this.body); this.visibility = this.visibility-5; push(); tint(255,this.visibility);
+         image(this.image,pos.x,pos.y,30,40); pop() }
     }
 }
