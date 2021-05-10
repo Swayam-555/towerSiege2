3c8218918a8f9b6218cee6ff56f780c44b5aedbf
Swayam-555 <radheshyam.panigrahi@gmail.com>
2021-05-10 20:45:36 +0530

    rotating and vanishing

--- Block.js
+++ Block.js
@@ -16,7 +16,7 @@ class Block{
       var pos= this.body.position;
       var angle = this.body.angle;
      
-      if(this.body.speed < 3){ push(); angleMode(RADIANS); translate(pos.x,pos.y); rotate(angle); rectMode(CENTER); rect(0,0,this.width, this.height); pop(); } else{ World.remove(world,this.body); this.visibility = this.visibility-5; push(); tint(255,this.visibility);
+      if(this.body.speed < 5.2){ push(); angleMode(RADIANS); translate(pos.x,pos.y); rotate(angle); rectMode(CENTER); rect(0,0,this.width, this.height); pop(); } else{ World.remove(world,this.body); this.visibility = this.visibility-5; push(); tint(255,this.visibility);
          image(this.image,pos.x,pos.y,30,40); pop() }
     }
 }
